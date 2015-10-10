6ac54c04e4250e7de15c00d1cf1ad6847dda3a56
shenxinle <shenxinle2009@163.com>
2015-10-10 14:47:25 +0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('style/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({
+            broswers: ['> 1%', 'ie 9', 'firefox > 18', 'safari 5', 'iso 6', 'android 3']
+        }))
         .pipe(gulp.dest('style'));
 });
 
